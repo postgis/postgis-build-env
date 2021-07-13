4cbf9a49b23c194705a3f3c5e7e154448898e35c
Regina Obe <4734450+robe2@users.noreply.github.com>
2021-07-13 01:26:03 -0400

    Add scheduled (#14)
    * Set to run at different periods
    
    * Fix typo
    
    * Fix typo
    
    * Add some instructions and jenkins build badge
    
    * Fix errors in logic

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 Scripts to generate Docker images with build environments for PostGIS.
 
 Generated images are hosted at [Docker Hub](https://hub.docker.com/r/postgis/postgis-build-env/tags/).
+
+To use:
+Builds all
+```
+python3 build.py 
+```
+
+Builds ones marked as frequently changing.  This is the command the jenkins bot uses for building.
+```
+python3 build.py weekly
+```
+
+Build progress is here - https://debbie.postgis.net/job/PostGIS-build-env/
+
+[![Build Status](https://debbie.postgis.net/buildStatus/icon?job=PostGIS-build-env%2Flabel%3Ddocker)](https://debbie.postgis.net/job/PostGIS-build-env/label=docker/)
--- build.py
+++ build.py
@@ -2,90 +2,109 @@
 import datetime
 import re
 import subprocess
+import sys
+env_batch = 'all'
+environments = []
+all_environments=[
+    # put last modified first to iterate faster
+        dict(
+            name='latest',
+            PG='master',
+            GEOS='main',
+            GDAL='master',
+            PROJ='master',
+            PG_CC='gcc'
+        ),
+        dict(
+            name='stable_pg14',
+            PG='REL_14_STABLE',
+            GEOS='main',
+            GDAL='release/3.1',
+            PROJ='7.1',
+            PG_CC='clang'
+        ),
+        dict(
+            name='stable_pg14',
+            PG='REL_14_STABLE',
+            GEOS='3.9',
+            GDAL='release/3.1',
+            PROJ='7.1',
+            PG_CC='gcc'
+        ),
+        dict(
+            name='stable_pg13',
+            PG='REL_13_STABLE',
+            GEOS='3.9',
+            GDAL='release/3.1',
+            PROJ='7.1',
+            PG_CC='clang'
+        ),
+        dict(
+            name='stable_pg13',
+            PG='REL_13_STABLE',
+            GEOS='3.9',
+            GDAL='release/3.1',
+            PROJ='7.1',
+            PG_CC='gcc'
+        ),
+        dict(
+            name='stable_pg12',
+            PG='REL_12_STABLE',
+            GEOS='3.8',
+            GDAL='release/3.0',
+            PROJ='6.1.1',
+            PG_CC='clang'
+        ),
+        dict(
+            name='stable_pg12',
+            PG='REL_12_STABLE',
+            GEOS='3.8',
+            GDAL='release/3.0',
+            PROJ='6.1.1',
+            PG_CC='gcc'
+        ),
+        dict(
+            name='stable_pg11',
+            PG='REL_11_STABLE',
+            GEOS='3.7',
+            GDAL='release/2.4',
+            PROJ='5.2',
+            PG_CC='gcc'
+        ),
+        dict(
+            name='stable_pg10',
+            PG='REL_10_STABLE',
+            GEOS='svn-3.6',
+            GDAL='release/2.3',
+            PROJ='4.9',
+            PG_CC='gcc'
+        ),
+        dict(
+            name='stable_pg96',
+            PG='REL9_6_STABLE',
+            GEOS='svn-3.6',
+            GDAL='release/2.2',
+            PROJ='4.9',
+            PG_CC='gcc'
+        ),
+        dict(
+            name='old_pg95',
+            PG='REL9_5_STABLE',
+            GEOS='svn-3.6',
+            GDAL='release/2.1',
+            PROJ='4.9',
+            PG_CC='gcc'
+        )
+    ]
+if len(sys.argv) > 1:    
+    env_batch =  sys.argv[1]
 
-environments=[
-# put last modified first to iterate faster
-    dict(
-        name='latest',
-        PG='master',
-        GEOS='main',
-        GDAL='master',
-        PROJ='master',
-        PG_CC='gcc'
-    ),
-    dict(
-        name='stable_pg14',
-        PG='REL_14_STABLE',
-        GEOS='3.9',
-        GDAL='release/3.1',
-        PROJ='7.1',
-        PG_CC='gcc'
-    ),
-    dict(
-        name='stable_pg13',
-        PG='REL_13_STABLE',
-        GEOS='3.9',
-        GDAL='release/3.1',
-        PROJ='7.1',
-        PG_CC='clang'
-    ),
-    dict(
-        name='stable_pg13',
-        PG='REL_13_STABLE',
-        GEOS='3.9',
-        GDAL='release/3.1',
-        PROJ='7.1',
-        PG_CC='gcc'
-    ),
-    dict(
-        name='stable_pg12',
-        PG='REL_12_STABLE',
-        GEOS='3.8',
-        GDAL='release/3.0',
-        PROJ='6.1.1',
-        PG_CC='clang'
-    ),
-    dict(
-        name='stable_pg12',
-        PG='REL_12_STABLE',
-        GEOS='3.8',
-        GDAL='release/3.0',
-        PROJ='6.1.1',
-        PG_CC='gcc'
-    ),
-    dict(
-        name='stable_pg11',
-        PG='REL_11_STABLE',
-        GEOS='3.7',
-        GDAL='release/2.4',
-        PROJ='5.2',
-        PG_CC='gcc'
-    ),
-    dict(
-        name='stable_pg10',
-        PG='REL_10_STABLE',
-        GEOS='svn-3.6',
-        GDAL='release/2.3',
-        PROJ='4.9',
-        PG_CC='gcc'
-    ),
-    dict(
-        name='stable_pg96',
-        PG='REL9_6_STABLE',
-        GEOS='svn-3.6',
-        GDAL='release/2.2',
-        PROJ='4.9',
-        PG_CC='gcc'
-    ),
-    dict(
-        name='old_pg95',
-        PG='REL9_5_STABLE',
-        GEOS='svn-3.6',
-        GDAL='release/2.1',
-        PROJ='4.9',
-        PG_CC='gcc'
-    )
-]
+print("Env Batch selected:", env_batch, all_environments[0:3])
+
+if env_batch == 'weekly': 
+    environments = all_environments[0:3]
+else:
+    environments = all_environments
 
 for env in environments:
     if env['PG_CC'] == 'clang':
